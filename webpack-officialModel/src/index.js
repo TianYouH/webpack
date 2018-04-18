a003b00f7b15f1ddc7d6346f254a856208c20741
LAPTOP-28A15GVN\黄金亮 <1406957473@qq.com>
2018-04-19 01:10:38 +0800

    临时提交

--- webpack-officialModel/src/index.js
+++ webpack-officialModel/src/index.js
@@ -1,11 +1,23 @@
-async function getComponent(params) {
-  var element = document.createElement('div');
-  const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
-  element.innerHTML = _.join(['你好', 'webpack'], ' ');
-  return element;
+function getComponent() {
+  return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+    var element = document.createElement('div');
+    // element.innerHTML = _.join(['你好', 'webpack'], ' ');
+    console.log(_.join(['你好', 'webpack'], ' '))
+    element.innerHTML = '123135456'
+    return element;
+  }).catch(error => {
+    console.log('加载组建错误')
+  })
 }
+
+// async function getComponent(params) {
+//   var element = document.createElement('div');
+//   const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+//   element.innerHTML = _.join(['你好', 'webpack'], ' ');
+//   return element;
+// }
 
 getComponent().then(component => {
   console.log(component)
-  document.body.appendChild(component);
+  document.body.appendChild(component || '<p>12345</p>');
 })
--- webpack-officialModel/webpack.config.js
+++ webpack-officialModel/webpack.config.js
@@ -7,38 +7,44 @@ const webpack = require('webpack');
 module.exports = {
   mode: 'development',
   entry: {
-    app: ['babel-polyfill', './src/index.js']
+    app: ['babel-polyfill','./src/index.js']
     // another: './src/another-module.js'
   },
   output: {
     filename: 'js/[name]-bundle.js',
     path: path.resolve(__dirname, './dist'),
     chunkFilename: '[name].bundle.js',
-    // publicPath: '/' //publicPath 会在服务器脚本用到，确保文件资源能够在 http://localhost:??? 下正确访问
+    publicPath: '/' //publicPath 会在服务器脚本用到，确保文件资源能够在 http://localhost:??? 下正确访问
   },
-  // devtool: 'inline-source-map', //为了更容易地追踪错误和警告，JavaScript 提供了 source map 功能
-  // devServer: {  //在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件。
-  //   contentBase: './dist',
-  //   hot: true
+  devtool: 'inline-source-map', //为了更容易地追踪错误和警告，JavaScript 提供了 source map 功能
+  devServer: {  //在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件。
+    contentBase: './dist',
+    hot: true
+  },
+  // module: {
+  //   rules: [{
+  //     test: /\.js$/,
+  //     use: [
+  //       {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           presets: ['latest']
+  //         }
+  //       }
+  //     ],
+  //     exclude: path.resolve(__dirname, "./node_modules"),
+      
+  //   }, ]
   // },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use:[{loader:"babel-loader"}]
-      }
-    ]
-  },
   plugins: [
     new clearWebpackPlugin(['dist']),
-    new uglifyJSPlugin(),
+    // new uglifyJSPlugin(),
     new htmlWebpackPlugin({
       //模板为同级目录下的index.html，为何不用写路径，是因为默认上下文问webpack.config.js所在的文件夹
-      template: 'index.html',  //自动生成HTML文件的名字
+      template: 'index.html', //自动生成HTML文件的名字
       //  filename:'index-[hash].html',  //引入打包后的js的script标签所在的位置,这里表示放在head标签中
       //  inject:'head',  //可以向模板传递参数，然后应用于自动生成的html,(模板需要获取参数)
-      title: '黄金亮你好帅啊',  //任何的参数都是可以传递的
+      title: '黄金亮你好帅啊', //任何的参数都是可以传递的
       date: new Date()
     }),
     // new webpack.optimize.SplitChunksPlugin({
@@ -65,7 +71,7 @@ module.exports = {
     //     }
     //   }
     // }),
-    // new webpack.NamedModulesPlugin(), //NamedModulesPlugin，以便更容易查看要修补(patch)的依赖
-    // new webpack.HotModuleReplacementPlugin()
+    new webpack.NamedModulesPlugin(), //NamedModulesPlugin，以便更容易查看要修补(patch)的依赖
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
